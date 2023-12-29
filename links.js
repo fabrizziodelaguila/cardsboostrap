7f085573df156ebb19e888a0cda87f93af896f0c
fabrizziodelaguila <fabrizzio.daguila@gmail.com>
2023-12-29 11:59:23 -0500

    feat: :art: mejora css y uso de js para botones
    mejora de botones con js

---
+++ links.js
@@ -0,0 +1,14 @@
+document.getElementById("botoncologne").addEventListener("click", function () {
+  window.open("https://www.hltv.org/events/6140/iem-cologne-2022", "_blank");
+});
+
+document.getElementById("botonkatowice").addEventListener("click", function () {
+  window.open("https://www.hltv.org/events/6136/iem-katowice-2022", "_blank");
+});
+
+document.getElementById("botonmajor").addEventListener("click", function () {
+  window.open(
+    "https://www.hltv.org/events/6372/pgl-major-antwerp-2022",
+    "_blank"
+  );
+});
