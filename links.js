624617a295834dd2f3585e78fcc4e245ba43a3df
fabrizziodelaguila <fabrizzio.daguila@gmail.com>
2023-12-29 12:45:07 -0500

    feat: añadir links en los items del banner
    links a boostrap

--- links.js
+++ links.js
@@ -12,3 +12,30 @@ document.getElementById("botonmajor").addEventListener("click", function () {
     "_blank"
   );
 });
+
+document.getElementById("itemboostrap").addEventListener("click", function () {
+  window.open("https://getbootstrap.com/", "_blank");
+});
+
+document.getElementById("itemdocs").addEventListener("click", function () {
+  window.open(
+    "https://getbootstrap.com/docs/5.3/getting-started/introduction/",
+    "_blank"
+  );
+});
+
+document.getElementById("itemexamples").addEventListener("click", function () {
+  window.open("https://getbootstrap.com/docs/5.3/examples/", "_blank");
+});
+
+document.getElementById("itemicons").addEventListener("click", function () {
+  window.open("https://icons.getbootstrap.com/", "_blank");
+});
+
+document.getElementById("itemthemes").addEventListener("click", function () {
+  window.open("https://themes.getbootstrap.com/", "_blank");
+});
+
+document.getElementById("itemblog").addEventListener("click", function () {
+  window.open("https://blog.getbootstrap.com/", "_blank");
+});
